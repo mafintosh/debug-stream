f994fbbb834cb0e2e71b9f92dba348ed8439ae7c
Mathias Buus <mathiasbuus@gmail.com>
2015-07-24 23:17:22 +0200

    forward the enabled property and style

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
 var debug = require('debug')
 var through = require('through2')
 
-module.exports = function(name) {
+module.exports = function (name) {
   var d = typeof name === 'function' ? name : debug(name)
 
   try {
-    var enabled = process.env.DEBUG || localStorage.debug
+    var enabled = !!(process.env.DEBUG || localStorage.debug)
   } catch (e) {}
 
-  if (!enabled) {
-    return function() {
-      return through.obj()
-    }
+  passThrough.enabled = false
+  debugStream.enabled = true
+
+  return enabled ? debugStream : passThrough
+
+  function passThrough () {
+    return through.obj()
   }
 
-  return function() {
+  function debugStream () {
     var args = arguments.length && Array.prototype.slice.call(arguments)
 
     var run = args ?
-      function(line) {
+      function (line) {
         d.apply(d, args.concat(line))
       } :
-      function(line) {
+      function (line) {
         d(line)
       }
 
-    return through.obj(function(data, enc, cb) {
+    return through.obj(function (data, enc, cb) {
       run(data)
       cb(null, data)
     })
